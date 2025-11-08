cb50da866956f0672f8b17986d2de64e4e734448
chris <cmseeling@gmail.com>
2025-11-08 09:37:09 -0600

    add basic structures for delay and pause range fieldsets

---
+++ apps/bluestart-web/src/lib/components/fieldsets/DelayFieldset.stories.svelte
@@ -0,0 +1,11 @@
+<script module lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import DelayFieldset from './DelayFieldset.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Components/DelayFieldset',
+		component: DelayFieldset
+	});
+</script>
+
+<Story name="Default" />
---
+++ apps/bluestart-web/src/lib/components/fieldsets/PauseRangeFieldset.stories.svelte
@@ -0,0 +1,11 @@
+<script module lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import PauseRangeFieldset from './PauseRangeFieldset.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Components/PauseRangeFieldset',
+		component: PauseRangeFieldset
+	});
+</script>
+
+<Story name="Default" />
