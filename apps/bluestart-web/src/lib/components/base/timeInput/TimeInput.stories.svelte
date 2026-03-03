3024b046d81e6a5a5b66d005eae11610bf5d3da9
chris <cmseeling@gmail.com>
2026-03-03 23:00:20 +0000

    add basic time input

---
+++ apps/bluestart-web/src/lib/components/base/timeInput/TimeInput.stories.svelte
@@ -0,0 +1,13 @@
+<script module>
+    import { defineMeta } from "@storybook/addon-svelte-csf";
+    import Input from './TimeInput.svelte';
+
+    const { Story } = defineMeta({
+        title: "Components/Base/TimeInput",
+        component: Input,
+    });
+</script>
+
+<Story name="Default" args={{ placeholder: "placeholder", type: "time" }} />
+
+<Story name="Error State" args={{ hasError: true }} />
---
+++ apps/bluestart-web/src/lib/components/base/timeInput/TimeInput.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { HTMLInputAttributes } from "svelte/elements";
+    // import { inputStyle } from '../input-style';
+    // import { cx } from 'styled-system/css';
+    import Input from '$lib/components/base/input/Input.svelte';
+
+    type Props = HTMLInputAttributes & {
+        class?: string;
+        hasError?: boolean;
+    };
+
+    let { value = $bindable(), ...props }: Props = $props();
+</script>
+
+<Input bind:value {...props} type="time" />
